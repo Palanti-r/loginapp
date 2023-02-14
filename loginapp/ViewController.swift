56ef710416562b991a54e725c4a9cbd9466ffdf0
Ilias <cepctapk@gmail.com>
2023-02-14 22:34:02 +0300

    Initial Commit

---
+++ loginapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginapp
+//
+//  Created by Commander on 14.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
